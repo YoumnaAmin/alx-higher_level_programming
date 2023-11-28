a115fd2f06511781eed3fe4cd79301b9568e0e3c
YoumnaAmin <youmnamin564@gmail.com>
2023-11-29 02:14:47 +0300

    ..

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str_res = ""
+    for i in str:
+        if (ord(i) >= 97 and ord(i) <= 122):
+            i = chr(ord(i) - 32)
+        str_res += i
+        print("{0:c}".format(i), end="")
+    print("\n")
