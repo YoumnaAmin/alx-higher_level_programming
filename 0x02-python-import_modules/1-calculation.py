e9ee69750225232d7a5ae2e6b70151d65bf374eb
YoumnaAmin <youmnamin564@gmail.com>
2023-12-01 02:26:37 +0300

    ..

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
     print("{0:d} - {1:d} = {2:d}".format(a, b, sub(a, b)))
     print("{0:d} * {1:d} = {2:d}".format(a, b, mul(a, b)))
-    print("{0:d} / {1:d} = {2:d}".format(a, b, div(a, b)))
+    print("{0:d} // {1:d} = {2:d}".format(a, b, div(a, b)))
