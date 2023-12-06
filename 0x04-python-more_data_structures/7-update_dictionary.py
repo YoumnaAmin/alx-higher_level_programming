95c14f97327f6a95a404c75eacb246e5f8f502c7
YoumnaAmin <youmnamin564@gmail.com>
2023-12-07 00:05:41 +0300

    ..

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for key in a_dictionary:
+        if a_dictionary.get(key):
+            a_dictionary[key] = value
+        else:
+            a_dictionary[key] = value
+    return a_dictionary
