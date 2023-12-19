dac50860be304f133261da7fa401354f2392966c
YoumnaAmin <youmnamin564@gmail.com>
2023-12-20 02:45:58 +0300

    ..

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" defines a square by: (based on 0-square.py"""
+
+
+class Square:
+    """ defines a square by: (based on 0-square.py"""
+    def __init__(self, size):
+        """ Class Constructor"""
+        self.__size = size
