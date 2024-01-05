3cced45381698db5683b86f1e3c1d008717a5d10
YoumnaAmin <youmnamin564@gmail.com>
2024-01-05 03:27:39 +0300

    ..

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Rectangle dimensions"""
+
+
+class Rectangle:
+    """Rectangle dimensions"""
+    def __init__(self, width=0, height=0):
+        """Class constructor"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter method for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """to calculate the area"""
+        return self.__height * self.__width
+    def perimeter(self):
+        """to calculate the perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2* self.__width) + (2* self.__height)
