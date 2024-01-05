14af3abb82a478d4879c73b541f18fb4b02cb7ed
YoumnaAmin <youmnamin564@gmail.com>
2024-01-05 03:09:48 +0300

    ..

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Rectangle dimensions"""
+
+
 class Rectangle:
     """Rectangle dimensions"""
-
     def __init__(self, width=0, height=0):
         """Class constructor"""
         self.__width = width
