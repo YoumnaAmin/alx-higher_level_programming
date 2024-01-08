6fa6803ead6253816cca3e6a0f6a234515f7c1bb
YoumnaAmin <youmnamin564@gmail.com>
2024-01-09 02:44:54 +0300

    ..

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,7 @@
 
 def inherits_from(obj, a_class):
     """Only sub class of"""
-    return issubclass(obj, a_class)
+    if type(obj) == a_class:
+        return False
+    else:
+        return isinstance(obj, a_class)
