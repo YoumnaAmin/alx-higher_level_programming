0fb9b7c8ba6c80a15fa0a747762214cc667ba081
YoumnaAmin <youmnamin564@gmail.com>
2024-01-10 02:58:57 +0300

    ..

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""function that appends a string"""
+
+
 def append_write(filename="", text=""):
+    """function that appends a string"""
     with open(filename, "a", encoding="UTF8") as f:
         return (f.write(text))
