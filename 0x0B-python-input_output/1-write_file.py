d4bbaf8bb6c26cfe9f32e54973727e138986ba6b
YoumnaAmin <youmnamin564@gmail.com>
2024-01-10 02:51:46 +0300

    ..

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file"""
+    with open(filename, "w", encoding="UTF8") as f:
+        return(f.write(text))
