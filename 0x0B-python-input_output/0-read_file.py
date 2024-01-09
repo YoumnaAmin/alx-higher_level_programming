d79ce44eb9cc9a53551e4cdb74f649f12e0daf21
YoumnaAmin <youmnamin564@gmail.com>
2024-01-10 02:44:20 +0300

    ..

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ to read and print a file content
 
 def read_file(filename=""):
     """to read and print a file content"""
-    with open (filename, encoding="UTF8") as f:
+    with open(filename, encoding="UTF8") as f:
         for line in f:
             print(line, end='')
