1f727629024e383af6657c969086ab6a9517b7ab
YoumnaAmin <youmnamin564@gmail.com>
2024-01-10 03:11:53 +0300

    ..

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" returns the JSON representation"""
+
+
+import json
+def to_json_string(my_obj):
+    """ returns the JSON representation """
+    return (json.dumps(my_obj))
