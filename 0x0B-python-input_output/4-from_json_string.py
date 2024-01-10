62c22858e6daaf24953e75930730702342f82751
YoumnaAmin <youmnamin564@gmail.com>
2024-01-10 03:15:19 +0300

    ..

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""function that returns an object"""
+
+import json
+
+
+def from_json_string(my_str):
+    """function that returns an object"""
+    return (json.loads(my_str))
