f9deedaeacd82aa441f1283ec0be3d60d1b6e1ee
YoumnaAmin <youmnamin564@gmail.com>
2024-01-14 23:11:52 +0300

    ..

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,9 +2,11 @@
 """ will be the “base” of all other classes"""
 
 
+import json
+
+
 class Base:
     """ will be the “base” of all other classes"""
-
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -14,3 +16,10 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            NewListDic = json.dumps(list_dictionaries)
+            return NewListDic
