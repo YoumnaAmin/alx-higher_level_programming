b4fd5932ed9806463e5f67711d72b7c5b5f1d64e
YoumnaAmin <youmnamin564@gmail.com>
2024-02-15 01:32:35 +0300

    ..

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- t
 -- u
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT on *.* to 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,2 @@
 -- t
 -- u
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';
