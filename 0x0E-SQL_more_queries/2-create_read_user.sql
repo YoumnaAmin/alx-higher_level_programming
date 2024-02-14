9ba0debb689c57212c8d789eaf2e755ea603e7a2
YoumnaAmin <youmnamin564@gmail.com>
2024-02-15 01:29:54 +0300

    ..

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- t
 -- u
-CREATE USER 'user_0d_1'@'localhost';
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT SELECT on *.* to 'user_0d_1'@'localhost';
