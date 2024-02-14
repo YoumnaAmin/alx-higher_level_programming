2a614f32b1cdfdafe083ce7cfc90e801b9d06c18
YoumnaAmin <youmnamin564@gmail.com>
2024-02-15 01:39:38 +0300

    ..

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,5 @@
 -- t
--- u
+-- x
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *.* to 'user_0d_2'@'localhost';
