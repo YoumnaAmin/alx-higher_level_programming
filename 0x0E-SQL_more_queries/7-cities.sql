b91c36f4462ffa148f791eab58dfe8fc0c849b33
YoumnaAmin <youmnamin564@gmail.com>
2024-02-15 02:43:19 +0300

    ..

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,10 @@
-#-- t
-#-- u
-
+-- Creates the database hbtn_0d_usa
+-- Query to perform operation
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+       id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       state_id INT NOT NULL PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
+       FOREIGN KEY (state_id) REFERENCES states (id)
+);
