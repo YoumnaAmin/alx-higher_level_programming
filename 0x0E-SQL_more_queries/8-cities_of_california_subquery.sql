efb9b3ca94110052907c8991454d49eab90bf34f
YoumnaAmin <youmnamin564@gmail.com>
2024-02-15 03:37:25 +0300

    ..

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,10 @@
-#-- t
-#-- u
-
+-- t
+-- u
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
