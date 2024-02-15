3882115057462e27cdf8fa32f7b9642a5c2d022d
YoumnaAmin <youmnamin564@gmail.com>
2024-02-15 04:31:45 +0300

    ..

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,8 @@
-#-- t
-#-- u
-
+-- t
+-- u
+SELECT gr.name AS genre,
+       COUNT(shgr.genre_id) AS number_of_shows FROM tv_genres AS gr
+       JOIN tv_show_genres AS shgr
+       ON gr.id=shgr.genre_id
+       GROUP BY shgr.genre_id
+       ORDER BY number_of_shows DESC;
