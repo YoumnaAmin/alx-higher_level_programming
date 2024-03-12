240c57738fd33462bb2a4f518f91406ac7a7b74b
YoumnaAmin <youmnamin564@gmail.com>
2024-03-12 04:14:34 +0300

    ,,

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const arg = process.argv;
 if (arg.length < 3) {
     console.log('No argument');
-  } else if (argv.length === 3) {
+  } else if (arg.length === 3) {
     console.log('Argument found');
   } else {
     console.log('Arguments found');
