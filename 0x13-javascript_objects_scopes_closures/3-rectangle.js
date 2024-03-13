b95f9e724e8803ca111407e497365941684244fe
YoumnaAmin <youmnamin564@gmail.com>
2024-03-13 03:43:19 +0300

    ,,

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      return; // If width or height is not set, do not print
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
