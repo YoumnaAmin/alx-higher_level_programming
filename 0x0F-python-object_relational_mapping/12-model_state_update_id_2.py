0d1f1ee61646e92507e2a0cf377eb274825ce824
YoumnaAmin <youmnamin564@gmail.com>
2024-03-18 02:40:04 +0300

    ..

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""model state sqlalchemy"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(State.id == 2)\
+        .order_by(State.id)
+    records = query.first()
+    if records:
+        records.name = "New Mexico"
+        session.commit()
