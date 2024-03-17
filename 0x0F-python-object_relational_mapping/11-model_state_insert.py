8bb6f4dc60a61fe4b904bbfe1a1297fc23ead311
YoumnaAmin <youmnamin564@gmail.com>
2024-03-18 02:29:47 +0300

    ..

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""model state sqlalchemy"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    query = session.query(State).order_by(State.id.desc())
+    record = query.first()
+    print("{:d}".format(record.id))
