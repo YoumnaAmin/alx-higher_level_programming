c81336656d29446e87f82798d064596553fc54d3
YoumnaAmin <youmnamin564@gmail.com>
2024-03-18 01:44:20 +0300

    ..

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""model state sqlalchemy"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id)
+    records = query.first()
+
+    if records:
+        print("{:d}: {:s}".format(records.id, records.name))
+    else:
+        print("Nothing")
