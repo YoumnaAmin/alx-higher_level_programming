111dda45b002431952cf7f1755c95b1ae23db801
YoumnaAmin <youmnamin564@gmail.com>
2024-03-18 01:55:15 +0300

    ..

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""model state sqlalchemy"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    records = query.all()
+
+    for record in records:
+        print("{:d}: {:s}".format(record.id, record.name))
