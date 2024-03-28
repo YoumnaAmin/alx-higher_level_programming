044d3facb50da3ce9c650b985aa6800d9f70f3e7
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 02:31:44 +0300

    ..

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Error code module"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            pass
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
