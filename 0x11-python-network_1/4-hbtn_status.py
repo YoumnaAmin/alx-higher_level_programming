12d33d83e5bdcd7bfd1d9520c9799d49660bb74e
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 02:44:57 +0300

    ..

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""hbtn status"""
+
+
+if __name__ == "__main__":
+    import requests
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    req = r.text
+    print("Body response:")
+    print("\t- type:", type(req))
+    print("\t- content:", req)
