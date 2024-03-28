4b4f53757b2b5bbd8f24d61fd0eff4ace121c113
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 02:35:37 +0300

    ..

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,10 +4,10 @@
 
 if __name__ == "__main__":
     import urllib.request
-    from urllib.error import HTTPError
     import sys
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
-            pass
+            body = response.read().decode('utf-8')
+            print(body)
     except urllib.error.HTTPError as e:
         print("Error code:", e.code)
