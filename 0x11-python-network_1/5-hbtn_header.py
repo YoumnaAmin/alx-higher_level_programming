cadc6eb92d3a6dd6e7fae106143a5950a993b260
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 02:49:19 +0300

    ..

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""hbtn_header module"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    ID = r.headers['X-Request-Id']
+    print(ID)
