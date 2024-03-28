56ec8070cbf625300cf0c0ad69822fc2834a852c
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 02:26:34 +0300

    ..

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import urllib.parse
     import urllib.request
     import sys
-    email = {'email' : sys.argv[2]}
+    email = {'email': sys.argv[2]}
     email_enc = urllib.parse.urlencode(email)
     email_enc = email_enc.encode('ascii')
     req = urllib.request.Request(sys.argv[1], email_enc)
