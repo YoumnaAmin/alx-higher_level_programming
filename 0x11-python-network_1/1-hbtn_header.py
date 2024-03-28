c7090f591ef9f519a86fdf55dddd0e9698f336c0
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 02:02:02 +0300

    ..

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""hbtn_header module"""
+
+
+import urllib.request
+import sys
+
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    id = response.headers.get('X-Request-Id')
+    print(id)
