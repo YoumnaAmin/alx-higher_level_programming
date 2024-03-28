5ddd8268227a5169917bddf3f11a91c1cc05cbdd
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 02:25:28 +0300

    ..

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""post email"""
+
+if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
+    email = {'email' : sys.argv[2]}
+    email_enc = urllib.parse.urlencode(email)
+    email_enc = email_enc.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], email_enc)
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
