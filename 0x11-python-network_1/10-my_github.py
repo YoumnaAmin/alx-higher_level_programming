0a92488def07bc215d4440b2298b019056967f79
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 04:28:12 +0300

    ..

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""my github module"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    headers = {'Authorization':
+               'github_pat_11BBM752Y0gxnEiz1Diti5\
+                _qLpHn1SdtWkc591WajLWdYS3Z8lq8vkHLWW0h2ijeNt3RIHHR3PQ9462ypq'}
+    r = requests.get('https://api.github.com/YoumnaAmin', headers=headers)
+    data = r.json()
+    ID = data.get('id')
+    print(ID)
