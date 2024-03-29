12f43d42f524670b0dddc2a64da495a8de9282de
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 03:00:00 +0300

    ..

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""post email"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    email = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=email)
+    body = r.text
+    print(body)
