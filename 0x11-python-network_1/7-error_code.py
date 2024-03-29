ffb4fd1c21a55ff533bf27a21e943aa6841e5850
YoumnaAmin <youmnamin564@gmail.com>
2024-03-29 03:04:25 +0300

    ..

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Error code module"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    else:
+        print(r.text)
